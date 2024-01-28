2f3e95b3f47102071afc13165f5bf67d89ae4937
Pawel Pyc <pawel.pyc@techsterowniki.pl>
2024-01-28 19:01:02 +0100

    t

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { lucia } from 'lucia';
-import { mongoose as mongooseAdapter } from '@lucia-auth/adapter-mongoose';
-import { express } from 'lucia/middleware';
-import { KeyModel } from '../models/key.model';
-import { UserModel } from '../models/user.model';
-import { SessionModel } from '../models/session.model';
+import { lucia } from "lucia";
+import { mongoose as mongooseAdapter } from "@lucia-auth/adapter-mongoose";
+import { express } from "lucia/middleware";
+import { KeyModel } from "../models/key.model";
+import { UserModel } from "../models/user.model";
+import { SessionModel } from "../models/session.model";
+import "lucia/polyfill/node";
 
 export const auth = lucia({
   adapter: mongooseAdapter({
@@ -15,11 +16,11 @@ export const auth = lucia({
     // @ts-expect-error
     Session: SessionModel,
   }),
-  env: 'DEV',
+  env: "DEV",
   middleware: express(),
   csrfProtection: false,
 
-  getUserAttributes: data => {
+  getUserAttributes: (data) => {
     return {
       username: data.username,
     };
