2d698a1b9c08a52edabc40be62f0ca5e82710660
Pawel Pyc <pawel.pyc@techsterowniki.pl>
2024-01-28 17:43:20 +0100

    hotp

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     ...VitePluginNode({
       adapter: 'express',
-      appPath: './src/server.ts',
+      appPath: './src/index.ts',
       exportName: 'viteNodeApp',
       tsCompiler: 'esbuild',
       swcOptions: {},
